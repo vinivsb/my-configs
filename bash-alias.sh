45f5e5738cdd5b3ccbafa9c5084f224e9235209f
Vinicius Paciencia <vinicius.concrete@corp.globo.com>
2015-04-06 15:12:21 -0300

    add some commands

---
+++ bash-alias.sh
@@ -0,0 +1,2 @@
+ "$HOME/.rvm/scripts/rvm" ]] && source "$HOME/.rvm/scripts/rvm" # Load RVM into a shell session *as a function*
+alias gem_deps="gem dependency -R"
---
+++ git-alias.sh
@@ -0,0 +1,7 @@
+alias.st=status -s
+alias.lg=log --graph --pretty=format:'%Cred%h%Creset -%C(yellow)%d%Creset %s %Cgreen(%cr) %C(bold blue)<%an>%Creset' --abbrev-commit --date=relative
+alias.aa=add . 
+alias.who=shortlog -sn 
+alias.undo=reset --hard HEAD 
+alias.ld=log --stat --graph
+alias.compare=git diff --stat --color

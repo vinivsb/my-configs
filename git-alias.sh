87d9f1edb7a66fb1b8fd524ebb5e1249e496eaf7
Vinicius Paciencia <vinicius.concrete@corp.globo.com>
2015-04-08 16:04:19 -0300

    tema para zshrc

--- git-alias.sh
+++ git-alias.sh
@@ -1,7 +1,11 @@
-alias.st=status -s
-alias.lg=log --graph --pretty=format:'%Cred%h%Creset -%C(yellow)%d%Creset %s %Cgreen(%cr) %C(bold blue)<%an>%Creset' --abbrev-commit --date=relative
-alias.aa=add . 
-alias.who=shortlog -sn 
-alias.undo=reset --hard HEAD 
-alias.ld=log --stat --graph
-alias.compare=git diff --stat --color
+add_aliases(){
+git config --global alias.st "status -s"
+git config --global alias.lg "log --graph --pretty=format:'%Cred%h%Creset -%C(yellow)%d%Creset %s %Cgreen(%cr) %C(bold blue)<%an>%Creset' --abbrev-commit --date=relative"
+git config --global alias.aa "add ."
+git config --global alias.who "shortlog -sn"
+git config --global alias.undo "reset --hard HEAD"
+git config --global alias.ld "log --stat --graph"
+git config --global alias.compare "git diff --stat --color"
+}
+
+add_aliases
---
+++ mytheme.zsh
@@ -0,0 +1,7 @@
+PROMPT='
+$fg[cyan]%n: $fg[yellow]%~ $fg[red]$(git_prompt_info)
+$fg[cyan]➣ $reset_color '
+
+ZSH_THEME_GIT_PROMPT_PREFIX="("
+ZSH_THEME_GIT_PROMPT_SUFFIX=")"
+

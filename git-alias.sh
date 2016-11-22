5b725f586b2b9de43330a96ab5c3d179c8874c3c
vinivsb <vinicius.vsb@gmail.com>
2016-11-22 11:21:53 -0200

    Update git-alias.sh
    alias pr removed

--- git-alias.sh
+++ git-alias.sh
@@ -6,7 +6,6 @@ git config --global alias.who "shortlog -sn"
 git config --global alias.undo "reset --hard HEAD"
 git config --global alias.ld "log --stat --graph"
 git config --global alias.compare "git diff --stat --color"
-git config --global alias.pr "git pull --rebase origin master"
 }
 
 add_aliases
